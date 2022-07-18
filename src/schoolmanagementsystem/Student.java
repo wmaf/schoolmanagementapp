dd46e48dd5d002fb828a26a323e85ea0e446581e
wmaf <wmaf@outlook.com>
2022-07-18 16:55:39 -0400

    Update project

--- src/schoolmanagementsystem/Student.java
+++ src/schoolmanagementsystem/Student.java
@@ -2,10 +2,48 @@ package schoolmanagementsystem;
 
 public class Student {
 
+    private int id;
+    private String name;
+    private int grade;
+    private int feesPaid;
+    private int fesTotal;
 
 
+    public Student(int id, String name, int grade, int feesPaid, int fesTotal) {
+        this.id = id;
+        this.name = name;
+        this.grade = grade;
+        this.feesPaid = 0;
+        this.fesTotal = 30000;
+    }
 
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
 
+    public void updateFeesPaid(int fees) {
 
+        feesPaid += fees;
+    }
 
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public int getFeesPaid() {
+        return feesPaid;
+    }
+
+    public int getFesTotal() {
+        return fesTotal;
+    }
 }
+
--- src/schoolmanagementsystem/Teacher.java
+++ src/schoolmanagementsystem/Teacher.java
@@ -2,11 +2,33 @@ package schoolmanagementsystem;
 
 public class Teacher {
 
+    private int id;
+    private String name;
+    private int salary;
 
+    public Teacher(int id, String name, int salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
 
+    public int getId() {
+        return id;
+    }
 
+    public String getName() {
+        return name;
+    }
 
+    public int getSalary() {
+        return salary;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 
-
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
 }
